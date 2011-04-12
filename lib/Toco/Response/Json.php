7b44d5a748fad061b9d0fbcd9e89742e5aab1464
Amitay Horwitz <amitay@babylon.com>
2011-04-12 13:39:16 +0300

    Modified default response objects (for plain and json)

--- lib/Toco/Response/Json.php
+++ lib/Toco/Response/Json.php
@@ -3,8 +3,10 @@
 class Toco_Response_Json extends Toco_Response
 {
 
-    public function __construct($content = null, $statusCode = 200, $contentType = 'application/json', $charset = 'utf-8') {
-        parent::__construct(json_encode($content), $statusCode, $contentType, $charset);
+    public $contentType = 'application/json';
+
+    public function __construct($content = null, $statusCode = 200, $charset = 'utf-8') {
+        parent::__construct(json_encode($content), $statusCode, $this->contentType, $charset);
     }
 
 }
