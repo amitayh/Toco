d30560584b20fcfa17f9f6e5511d6fb8abe5942d
Amitay Horwitz <amitay@babylon.com>
2011-05-25 10:10:20 +0300

    Minor fix

--- example/app/config.php
+++ example/app/config.php
@@ -2,7 +2,7 @@
 
 // General config
 define('DEBUG', true);
-define('APPLICATION_PATH', dirname(__file__));
+define('APPLICATION_PATH', dirname(__FILE__));
 define('TEMPLATES_PATH', APPLICATION_PATH . '/templates');
 define('CACHE_PATH', APPLICATION_PATH . '/cache');
 define('TEMPLATE_ADAPTER', 'Toco_Template_Adapter_Twig');
